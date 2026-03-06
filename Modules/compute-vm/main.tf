bb69bddf83abe82943192a4b0d0ab4f4e207b0ef
hamza_nass <hamzanass20@gmail.com>
2026-03-05 19:55:34 -0500

    main ready

--- Modules/compute-vm/main.tf
+++ Modules/compute-vm/main.tf
@@ -10,10 +10,10 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "<ton_subscription_id>"
-  tenant_id       = "<ton_tenant_id>"
-  client_id       = "<ton_client_id>"
-  client_secret   = "<ton_client_secret>"
+  subscription_id = "06d74348-c318-424e-8a31-800750ae4260"
+  tenant_id       = "cffdc055-33fa-43e0-8036-5213f8a018ec"
+  client_id       = "5bea517a-59a6-47d3-870d-e4d50df6aa37"
+  client_secret   = "54a8Q~xZytCnFaYBZuklN0gNxNG~H8Y.j4lIjcXS"
 }
 
 
